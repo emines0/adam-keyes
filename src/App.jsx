f80306450e090739d60b1e742655200b45622b03
emines0 <emin.sefa1@gmail.com>
2023-12-04 08:59:28 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Contact from './components/Contact';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
